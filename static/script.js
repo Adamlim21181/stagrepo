0e5477fd0e2d1c9143b1bba1a5b6728bc4572308
Adamlimnz <adamlimnz@gmail.com>
2025-04-14 14:38:23 +1200

    results filter error

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll("#toggle-columns input").forEach(checkbox => {
     checkbox.addEventListener("change", function() {
         let columnIndex = this.dataset.col; // Get the column index from the checkbox
-        let cells = document.querySelectorAll(`#resultsTable th:nth-child(${+columnIndex+1}), #resultsTable td:nth-child(${+columnIndex+1})`);
+        let cells = document.querySelectorAll(`#results_table th:nth-child(${+columnIndex+1}), #results_table td:nth-child(${+columnIndex+1})`);
 
         // Show or hide the column based on checkbox state
         cells.forEach(cell => {
