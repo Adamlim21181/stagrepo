b3aea628cc816e409f75a1dae67200644b7c0bc3
Adamlimnz <adamlimnz@gmail.com>
2025-05-07 11:33:19 +1200

    error

--- create_app.py
+++ create_app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://STAGNASTICS:stagweb@2025!@STAGNASTICS.mysql.pythonanywhere-services.com/stagnastics$stagdata'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+    
+    return app
     
