ccbd63b09fee81d0f58aa4611da5259c2d43a7fa
Adamlimnz <adamlimnz@gmail.com>
2025-05-09 12:36:30 +1200

    e

--- create_app.py
+++ create_app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://STAGNASTICS:stagweb@2025!@STAGNASTICS.mysql.pythonanywhere-services.com/STAGNASTICS$stagdata'
+    connection = pymysql.connect(
+        host = 'STAGNASTICS.mysql.pythonanywhere-services.com',
+        user = 'STAGNASTICS',
+        password = 'stagweb@2025!',
+        db = 'STAGNASTICS$stagdata',
+        cursorclass = pymysql.cursors.DictCursor
+    )
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
